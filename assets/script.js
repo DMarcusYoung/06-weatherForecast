c05bd96ac54aebda064915c2ff5b6bf66ea22eb6
Marcus Young <monkeyyoung3@gmail.com>
2020-02-10 13:48:09 -0800

    Added search city function

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,41 @@
 $(document).ready(function(){
-    // const $search = $(".searchBtn");
+    const $displayDiv = ("#displayDiv");
+    const $city = $(".city");
+    const $temp = $(".temp");
+    const $humidity = $(".humidity");
+    const $wind = $(".wind");
+    const $uv = $(".uv");
+    
     $(document).on('click', ".searchBtn", function(){
         const $citySearch = $(".citySearch").val();
-        let url = "http://samples.openweathermap.org/data/2.5/weather?q=" + $citySearch + "&appid=b6907d289e10d714a6e88b30761fae22";
+        const $cityMenu = $("<li>");
+        
+        
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + $citySearch + "&units=imperial&APPID=89f1fcf0b3207d8d3c0dce4e4149158f"
         $.ajax({
             url,
             method: "GET"
-          }).then(function(response){
-              console.log(response);
-          })
+        }).then(function(response){
+            let uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon +"&APPID=89f1fcf0b3207d8d3c0dce4e4149158f"
+            $.ajax({
+                url: uvUrl,
+                method: "GET"
+            }).then(function(response2){
+                var date = new Date(response.dt * 1000).toDateString();
+                $cityMenu.addClass("list-group-item").text(response.name);
+                $('.list-group').append($cityMenu);
+
+                $city.text(`${response.name} (${date})`);
+                $temp.text(`${response.main.temp} °F`);
+                $humidity.text(`${response.main.humidity}%`);
+                $wind.text(`${response.wind.speed} MPH`);
+                $uv.text(response2.value);
+                console.log(response, response2);
+            })
+
+        })
+    })
+    $(document).on('click', ".list-group-item", function(){
+        
     })
 });
